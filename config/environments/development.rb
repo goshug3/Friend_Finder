e1bb20e24eccb16287f856624793be12153ae3e4
goshug3 <goshug3@gmail.com>
2022-05-21 07:54:34 +0000

    Add トップページ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "17877e17873f4740b576d250f43b6476.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
